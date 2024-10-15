60bd7c12da65311c75eccadd4bc6ca090b67a393
Thakarepradnyan <pradnyanthakare1943@gmail.com>
2024-10-16 01:03:27 +0530

    Model Trainer

--- src/Components/Data_Ingestion.py
+++ src/Components/Data_Ingestion.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.Components.Data_Transformation import DataTransformation
 from src.Components.Data_Transformation import DataTransformationConfig
-
+from src.Components.Model_Trainer import ModelTrainer
+from src.Components.Model_Trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -54,4 +55,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,10 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+
 
 import dill
 
@@ -15,5 +18,36 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(xtrain, ytrain, xtest, ytest, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(xtrain, ytrain)
+
+            model.set_params(**gs.best_params_)
+            model.fit(xtrain, ytrain)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(xtrain)
+
+            y_test_pred = model.predict(xtest)
+
+            train_model_score = r2_score(ytrain, y_train_pred)
+
+            test_model_score = r2_score(ytest, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
